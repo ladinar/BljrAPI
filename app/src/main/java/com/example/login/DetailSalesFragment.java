77fd572365b54ddf0a7ca32570fc6ee346ec7515
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2019-10-11 16:51:20 +0700

    Update SD 2

--- app/src/main/java/com/example/login/DetailSalesFragment.java
+++ app/src/main/java/com/example/login/DetailSalesFragment.java
@@ -1,6 +1,7 @@
 package com.example.login;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
@@ -39,8 +40,8 @@ import util.Server;
 
 public class DetailSalesFragment extends Fragment {
     TextView tvLead, tvOppName, tvLead2, tvpresales, tvproposed, tvassessment, tvproof, tvproject_size, tvpriority;
-    String lead_edit, etOppName2, etLead2, etassesment2, etproposed2, etproof2, etproject_budget2, etpriority, etproject_size, etNik2, tvassessment2, tvproposed2, tvproof2, tvproject_size2, tvpriority2;
-    EditText etLead, etassesment, etproposed, etproject_budget, etproof, etNik;
+    String lead_edit, etOppName2, etLead2, etassesment2, etproposed2, etproof2, etproject_budget2, etpriority, etproject_size, etNik2, tvassessment2, tvproposed2, tvproof2, tvproject_size2, tvpriority2, etassess_before2, etpro_before2, etproof_before2, etpb_before2, etprio_before2, etpj_before2, ettimeasses_before2, ettimepro_before2, ettimeproof_before2;
+    EditText etLead, etassesment, etproposed, etproject_budget, etproof, etNik, etassess_before, etpro_before, etproof_before, etpb_before, etprio_before, etpj_before, ettimeasses_before, ettimepro_before, ettimeproof_before;
     Spinner spinnerPriority, spinnerProjectSize;
     Button btnSubmitsd, btnTp;
     private OnFragmentInteractionListener mListener;
@@ -89,6 +90,24 @@ public class DetailSalesFragment extends Fragment {
         tvpriority2 = tvpriority.getText().toString().trim();
         tvproject_size = view.findViewById(R.id.tvproject_size);
         tvproject_size2 = tvproject_size.getText().toString().trim();
+        etassess_before = view.findViewById(R.id.tv_assessment_before);
+        etassess_before2 = etassess_before.getText().toString().trim();
+        etpro_before = view.findViewById(R.id.tv_propoosed_before);
+        etpro_before2 = etpro_before.getText().toString().trim();
+        etproof_before = view.findViewById(R.id.tv_proof_before);
+        etproof_before2 = etproof_before.getText().toString().trim();
+        etpb_before = view.findViewById(R.id.tv_pb_before);
+        etpb_before2 = etpb_before.getText().toString().trim();
+        etprio_before = view.findViewById(R.id.tv_prio_before);
+        etprio_before2 = etprio_before.getText().toString().trim();
+        etpj_before = view.findViewById(R.id.tv_pj_before);
+        etpj_before2 = etpj_before.getText().toString().trim();
+        ettimeasses_before = view.findViewById(R.id.ettimeasses_before);
+        ettimeasses_before2 = ettimeasses_before.getText().toString().trim();
+        ettimepro_before = view.findViewById(R.id.ettimepropose_before);
+        ettimepro_before2 = ettimepro_before.getText().toString().trim();
+        ettimeproof_before = view.findViewById(R.id.ettimeproof_before);
+        ettimeproof_before2 = ettimeproof_before.getText().toString().trim();
         btnSubmitsd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,63 +127,9 @@ public class DetailSalesFragment extends Fragment {
         /*etNik.setText(lead.getNik());
         etNik2 = etNik.getText().toString().trim();*/
 
-//        tampilsd();
-
         tampilkanpresales();
 
         return view;
-    }
-
-    private void tampilsd() {
-        final JSONObject jobj = new JSONObject();
-        final String presales = "null";
-        try {
-
-            jobj.put("spinnerProjectSize", etproject_size);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        final JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.POST, Server.URL_update_sd, jobj, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.i("response", response.toString());
-                JSONObject jObj = response;
-                String success = null;
-                try {
-                    success = jObj.getString("success");
-
-                    if (success.equals("1")) {
-                        Toast.makeText(getActivity(), "Solution Design Updated Successfully :)", Toast.LENGTH_LONG).show();
-
-                    } else {
-                        Toast.makeText(getActivity(), "salah!", Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-
-                    Toast.makeText(getActivity(), "Ora oleh data", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        NetworkResponse response = error.networkResponse;
-                        String errorMsg = "";
-                        if (response != null && response.data != null) {
-                            String errorString = new String(response.data);
-                            Log.i("log error", errorString);
-                        }
-                        Toast.makeText(getActivity(), "Error" + error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
-        requestQueue.add(strReq);
     }
 
     private void tampilkanpresales() {
@@ -172,6 +137,15 @@ public class DetailSalesFragment extends Fragment {
         final String presales = "null";
         try {
             jobj.put("etLead", etLead2);
+            jobj.put("ettimeasses_before", ettimeasses_before2);
+            jobj.put("ettimepro_before", ettimepro_before2);
+            jobj.put("ettimeproof_before", ettimeproof_before2);
+            jobj.put("etassess_before", etassess_before2);
+            jobj.put("etpro_before", etpro_before2);
+            jobj.put("etproof_before", etproof_before2);
+            jobj.put("etpb_before", etpb_before2);
+            jobj.put("etprio_before", etprio_before2);
+            jobj.put("etpj_before2", etpj_before2);
             jobj.put("etassesment", etassesment2);
             jobj.put("etproposed", etproposed2);
             jobj.put("etproof", etproof2);
@@ -205,6 +179,25 @@ public class DetailSalesFragment extends Fragment {
                         tvproof.setText(presales.getString("pov_dates"));
                         tvpriority.setText(presales.getString("prioritys"));
                         tvproject_size.setText(presales.getString("project_sizes"));
+                        etassess_before.setText(presales.getString("assessments"));
+                        etpro_before.setText(presales.getString("pds"));
+                        etproof_before.setText(presales.getString("povs"));
+                        etpb_before.setText(presales.getString("pbs"));
+                        etprio_before.setText(presales.getString("prioritys"));
+                        etpj_before.setText(presales.getString("project_sizes"));
+                        ettimeasses_before.setText(presales.getString("assessment_dates"));
+                        ettimepro_before.setText(presales.getString("pd_dates"));
+                        ettimeproof_before.setText(presales.getString("pov_dates"));
+                        etassess_before.setVisibility(View.GONE);
+                        etpro_before.setVisibility(View.GONE);
+                        etproof_before.setVisibility(View.GONE);
+                        etpb_before.setVisibility(View.GONE);
+                        etprio_before.setVisibility(View.GONE);
+                        etpj_before.setVisibility(View.GONE);
+                        ettimeproof_before.setVisibility(View.GONE);
+                        ettimepro_before.setVisibility(View.GONE);
+                        ettimeasses_before.setVisibility(View.GONE);
+
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -255,7 +248,9 @@ public class DetailSalesFragment extends Fragment {
                     success = jObj.getString("success");
 
                     if (success.equals("1")) {
+                        Intent intent = new Intent(getActivity(), LeadRegister.class);
                         Toast.makeText(getActivity(), "Solution Design Updated Successfully :)", Toast.LENGTH_LONG).show();
+                        startActivity(intent);
 
                     } else {
                         Toast.makeText(getActivity(), "salah!", Toast.LENGTH_LONG).show();
