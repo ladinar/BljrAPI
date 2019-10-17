89374356e72411f21c78d22728a01244817cb2fb
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2019-10-17 16:25:47 +0700

    Update Result

--- app/src/main/java/com/example/login/TenderFragment.java
+++ app/src/main/java/com/example/login/TenderFragment.java
@@ -1,5 +1,6 @@
 package com.example.login;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -35,7 +37,9 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import util.Server;
 
@@ -44,11 +48,12 @@ public class TenderFragment extends Fragment {
 
 
     private OnFragmentInteractionListener mListener;
-    Button btn_submit_tp;
+    Button btn_submit_tp, btn_result, btn_submit_result;
     EditText etno_doc, etsubmit_price, etdeal_price, etproject_name, submit_date, etLead, etquote;
-    Spinner spinproject_class, spinwin_prob;
+    Spinner spinproject_class, spinwin_prob, spinresult;
     DatePickerDialog.OnDateSetListener date;
     Calendar myCalendar;
+    TextView tvproject_class, tvwin_prob, tvLead;
     String etLead2, etno_doc2, etsubmit_price2, etdeal_price2, etproject_name2, etsubmit_date2, etquote2, etproject_class, etwin_prob;
 
 
@@ -77,17 +82,21 @@ public class TenderFragment extends Fragment {
         etsubmit_price.addTextChangedListener(onTextChangedListener());
         spinproject_class = view.findViewById(R.id.spinner_project_class);
         etdeal_price = view.findViewById(R.id.edittext_deal_price);
-        etdeal_price.addTextChangedListener(onTextChangedListener());
+        etdeal_price.addTextChangedListener(onTextChangedListener2());
         spinwin_prob = view.findViewById(R.id.spinner_win_prob);
         etproject_name = view.findViewById(R.id.edittext_project_name);
         etquote = view.findViewById(R.id.edittext_quote);
+        tvproject_class = view.findViewById(R.id.tvproject_class);
+        tvwin_prob = view.findViewById(R.id.tvwin_prob);
+        btn_result = view.findViewById(R.id.btn_result);
+
 
         btn_submit_tp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 etno_doc2 = etno_doc.getText().toString().trim();
-                etsubmit_price2 = etsubmit_price.getText().toString().trim();
-                etdeal_price2 = etdeal_price.getText().toString().trim();
+                etsubmit_price2 = etsubmit_price.getText().toString().trim().replaceAll(",", "");
+                etdeal_price2 = etdeal_price.getText().toString().trim().replaceAll(",", "");
                 etproject_name2 = etproject_name.getText().toString().trim();
                 etquote2 = etquote.getText().toString().trim();
                 etproject_class = spinproject_class.getSelectedItem().toString().trim();
@@ -112,7 +121,149 @@ public class TenderFragment extends Fragment {
                 new DatePickerDialog(getActivity(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tampilresult();
+            }
+        });
+
+
+        tampiltp();
         return view;
+    }
+
+    private void tampilresult() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+//        View mView = getActivity().getLayoutInflater().inflate(R.layout.add_result, null);
+        LayoutInflater inflater = getLayoutInflater();
+        View mView = inflater.inflate(R.layout.add_result, null);
+        mBuilder.setView(mView);
+
+        Leads lead = (Leads) getActivity().getIntent().getSerializableExtra(LeadRegister.LEADS1);
+
+        spinresult = mView.findViewById(R.id.spinner_result);
+        btn_submit_result = mView.findViewById(R.id.btn_submit_result);
+        tvLead = mView.findViewById(R.id.lead_id);
+
+
+        tvLead.setText(lead.getLead_id());
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        btn_submit_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                submit_result();
+            }
+        });
+    }
+
+    private void submit_result() {
+        String hasil = spinresult.getSelectedItem().toString().trim();
+        String lead_id = tvLead.getText().toString().trim();
+        final JSONObject jobj = new JSONObject();
+        try {
+            jobj.put("spinner_result", hasil);
+            jobj.put("lead_id", lead_id);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        final JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.POST, Server.URL_update_result, jobj, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("response", response.toString());
+                JSONObject jObj = response;
+                String success = null;
+                try {
+                    success = jObj.getString("success");
+
+                    if (success.equals("1")) {
+                        Intent intent = new Intent(getActivity(), LeadRegister.class);
+                        Toast.makeText(getActivity(), "Update Result Successfully :)", Toast.LENGTH_LONG).show();
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(getActivity(), "salah!", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(getActivity(), "Ora oleh data", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                NetworkResponse response = error.networkResponse;
+                String errorMsg = "";
+                if (response != null && response.data != null) {
+                    String errorString = new String(response.data);
+                    Log.i("log error", errorString);
+                }
+                Toast.makeText(getActivity(), "Error" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
+    }
+
+    private void tampiltp() {
+        final JSONObject jobj = new JSONObject();
+        final String tp_detail = "null";
+        try {
+            jobj.put("etLead", etLead2);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        final JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.POST, Server.URL_tampil_tp, jobj, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.i("response", response.toString());
+                    JSONObject jObj = response;
+                    String success = jObj.getString("success");
+                    if (success.equals("1")) {
+                        JSONObject tp_detail = jObj.getJSONObject("tp_detail");
+                        etno_doc.setText(tp_detail.getString("auction_numbers"));
+                        etsubmit_price.setText(tp_detail.getString("submit_prices"));
+                        etquote.setText(tp_detail.getString("quote_numbers2"));
+                        etproject_name.setText(tp_detail.getString("project_names"));
+                        submit_date.setText(tp_detail.getString("submit_dates"));
+                        tvwin_prob.setText(tp_detail.getString("win_probs"));
+                        etdeal_price.setText(tp_detail.getString("deal_prices"));
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("lead_id", tp_detail);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 
 
@@ -197,7 +348,6 @@ public class TenderFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
                 etsubmit_price.removeTextChangedListener(this);
-                etdeal_price.removeTextChangedListener(this);
 
                 try {
                     String originalString = s.toString();
@@ -216,13 +366,52 @@ public class TenderFragment extends Fragment {
                     //setting text after format to EditText
                     etsubmit_price.setText(formattedString);
                     etsubmit_price.setSelection(etsubmit_price.getText().length());
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                }
+
+                etsubmit_price.addTextChangedListener(this);
+            }
+        };
+    }
+
+    private TextWatcher onTextChangedListener2() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                etdeal_price.removeTextChangedListener(this);
+
+                try {
+                    String originalString = s.toString();
+
+                    Long longval;
+                    if (originalString.contains(",")) {
+                        originalString = originalString.replaceAll(",", "");
+                    }
+
+                    longval = Long.parseLong(originalString);
+
+                    DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+                    formatter.applyPattern("#,###,###,###");
+                    String formattedString = formatter.format(longval);
+
+                    //setting text after format to EditText
                     etdeal_price.setText(formattedString);
                     etdeal_price.setSelection(etdeal_price.getText().length());
                 } catch (NumberFormatException nfe) {
                     nfe.printStackTrace();
                 }
 
-                etsubmit_price.addTextChangedListener(this);
                 etdeal_price.addTextChangedListener(this);
             }
         };
--- app/src/main/java/util/Server.java
+++ app/src/main/java/util/Server.java
@@ -10,4 +10,6 @@ public class Server {
     public static final String URL_update_sd = "http://192.168.2.142/api/update_sd";
     public static final String URL_raise_to_tender = "http://192.168.2.142/api/raise_to_tender";
     public static final String URL_update_tp = "http://192.168.2.142/api/update_tp";
+    public static final String URL_tampil_tp = "http://192.168.2.142/api/tampil_tp";
+    public static final String URL_update_result = "http://192.168.2.142/api/update_result";
 }
